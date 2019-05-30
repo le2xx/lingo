b327bcd76a719c87dbf5f5e9a2c0ab6191727555
le2xx <le2x@rambler.ru>
2019-05-30 22:48:03 +0300

    Created content component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LeftSideBarComponent } from './components/left-side-bar/left-side-bar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WrapperComponent,
-    HeaderComponent
+    HeaderComponent,
+    LeftSideBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
